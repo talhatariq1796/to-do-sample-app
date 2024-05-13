dc4d991e499f7ca5648c536e8ad45849e5c15cc0
talhatariq1796 <talha@whiteboxtech.net>
2024-05-13 18:08:17 +0500

    feat: Update in design

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,8 +8,9 @@ import {
 } from "../redux/actions";
 import { CiEdit } from "react-icons/ci";
 import { MdDeleteOutline } from "react-icons/md";
-import { IoMdArrowDropright } from "react-icons/io";
+import { IoMdArrowDropright, IoMdClose } from "react-icons/io";
 import { MdArrowDropUp } from "react-icons/md";
+import { TiTick } from "react-icons/ti";
 
 const TodoList = ({
   todos,
@@ -70,7 +71,7 @@ const TodoList = ({
         {sortedTodos.map((todo) => (
           <div
             key={todo.id}
-            className="flex flex-col w-full  relative p-6  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            className="flex justify-between items-center w-full p-4 py-6 bg-[#FCFAFF] rounded-[10px]"
           >
             {editedTask && editedTask.id === todo.id ? (
               <form onSubmit={handleEditSubmit} className="w-full">
@@ -78,72 +79,88 @@ const TodoList = ({
                   type="text"
                   value={editedTitle}
                   onChange={(e) => setEditedTitle(e.target.value)}
-                  className="bg-gray-50 mb-5 border outline-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="w-full bg-gray-50 mb-5 border outline-none border-gray-300 rounded-lg text-black p-3 hover:border-[#9E78CF] focus:border-[#9E78CF]"
                 />
                 <textarea
                   value={editedDescription}
                   onChange={(e) => setEditedDescription(e.target.value)}
-                  className="bg-gray-50 border mb-5 resize-none outline-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="w-full bg-gray-50 border mb-5 resize-none outline-none text-sm rounded-lg  block p-3 border-gray-300 text-black hover:border-[#9E78CF] focus:border-[#9E78CF]"
                 ></textarea>
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-                >
-                  Save
-                </button>
-                <button
-                  type="button"
-                  onClick={handleCancelEdit}
-                  className="bg-gray-500 ml-5 text-white px-4 py-2 rounded-lg"
-                >
-                  Cancel
-                </button>
+                <div className="flex items-center justify-end gap-2 w-full">
+                  <button
+                    type="button"
+                    onClick={handleCancelEdit}
+                    className="bg-gray-500 text-white px-4 py-2 rounded-lg"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="bg-[#9E78CF] text-white px-4 py-2 rounded-lg"
+                  >
+                    Save
+                  </button>
+                </div>
               </form>
             ) : (
               <>
-                <h5
-                  style={{
-                    textDecoration: todo.completed ? "line-through" : "none",
-                  }}
-                  className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                >
-                  {todo.title}
-                </h5>
-
-                {todo.showDescription && (
-                  <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {todo.description}
-                  </p>
-                )}
-                <button
-                  className="absolute bottom-3 right-3 text-gray-600"
-                  onClick={() => handleToggleDescription(todo.id)}
-                >
-                  {todo.showDescription ? (
-                    <MdArrowDropUp fontSize={25} />
-                  ) : (
-                    <IoMdArrowDropright fontSize={25} />
+                <div>
+                  <div className="flex gap-2 items-center">
+                    <button
+                      className="text-gray-600 text-[24px] mb-[6px]"
+                      onClick={() => handleToggleDescription(todo.id)}
+                    >
+                      {todo.showDescription ? (
+                        <MdArrowDropUp />
+                      ) : (
+                        <IoMdArrowDropright />
+                      )}
+                    </button>
+                    <h5
+                      style={{
+                        textDecoration: todo.completed
+                          ? "line-through"
+                          : "none",
+                        color: todo.completed ? "#35A07A" : "black",
+                      }}
+                      className="mb-2 text-2xl font-bold tracking-tight text-black dark:text-white"
+                    >
+                      {todo.title}
+                    </h5>
+                  </div>
+                  {todo.showDescription && (
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                      {todo.description}
+                    </p>
                   )}
-                </button>
-                <div className="absolute top-3 right-1 flex items-center gap-2">
-                  <input
-                    type="checkbox"
-                    className="w-[20px] h-[15px] bg-black outline-none cursor-pointer"
-                    onClick={() => toggleTodo(todo.id)}
-                    checked={todo.completed}
-                  />
+                </div>
+
+                <div className="flex items-center gap-2">
+                  {!todo.completed ? (
+                    <TiTick
+                      className="cursor-pointer text-[#35A07A]"
+                      fontSize={25}
+                      onClick={() => toggleTodo(todo.id)}
+                    />
+                  ) : (
+                    <IoMdClose
+                      className="cursor-pointer text-red-700"
+                      fontSize={25}
+                      onClick={() => toggleTodo(todo.id)}
+                    />
+                  )}
+
                   {!todo.completed && (
                     <CiEdit
-                      color=""
                       onClick={() => handleEditClick(todo)}
-                      size={20}
+                      size={25}
                       className="cursor-pointer"
                     />
                   )}
                   <MdDeleteOutline
                     color="red"
                     onClick={() => removeTodo(todo.id)}
-                    size={20}
+                    size={25}
                     className="cursor-pointer"
                   />
                 </div>
